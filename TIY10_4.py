b9cb205f7033d7ab753b5ca42a60b4a16ed75fb4
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-10-08 13:23:32 -0400

    Finish Working TIY 3-5 Created guest.txt for these TIYs. Created guest_book.txt for these TIYs. Created programming_poll.txt for these TIYs.

---
+++ TIY10_4.py
@@ -0,0 +1,9 @@
+
+while True:
+    with open("guest_book.txt", "a") as file_object:
+        name = input("What is your name? ")
+        if name == "quit": break
+        file_object.write(name + "\n")
+    print(f"Greetings, {name}!")
+
+
---
+++ TIY10_5.py
@@ -0,0 +1,8 @@
+
+while True:
+    question = input("Why do you like programming? ")
+    if question == 'quit': break
+    with open("programming_poll.txt", "a") as f_o:
+        f_o.write(question + "\n")
+
+
