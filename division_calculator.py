9c09b25a4957f138e39313dfdfcfac7e96dee8e4
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-11-02 14:24:31 -0500

    Added a catch for dividing by zero.

--- division_calculator.py
+++ division_calculator.py
@@ -8,6 +8,9 @@ while True:
     second_number = input("Second Number: ")
     if second_number == "q":
         break
-
-    answer = int(first_number) / int(second_number)
-    print(answer)
+    try:
+        answer = int(first_number) / int(second_number)
+    except ZeroDivisionError:
+        print("You cannot divide by zero!")
+    else:
+        print(answer)
