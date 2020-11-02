c444f0d0d4402ead5a5e88c2df542f15c56c9e63
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-11-02 14:53:15 -0500

    Added get_new_username() function which gets the input from the client that is their name, and stores it in username.json and returns the username.
    This was something in greet_user(), we reworked it to use the new function.

--- remember_me.py
+++ remember_me.py
@@ -12,16 +12,21 @@ def get_stored_username():
         return username
 
 
+def get_new_username():
+    username = input("What is your name? ")
+    filename = 'username.json'
+    with open(filename, "w+") as f:
+        json.dump(username, f)
+    return username
+
+
 def greet_user():
     username = get_stored_username()
     if username:
         print(f"Welcome back, {username}!")
     else:
-        username = input("What is your name? ")
-        filename = "username.json"
-        with open(filename, "w+") as f:
-            json.dump(username, f)
-            print(f"We'll remember you when you come back, {username}!")
+        username = get_new_username()
+        print(f"We'll remember you when you come back, {username}!")
 
 
 greet_user()
