1246b15028970e335032b3245fc9d30605e3903b
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-11-02 14:50:48 -0500

    Added new function get_stored_username() which does as the name entails. This was some programming that was in the greet_user function, so that was reworked to use the new method aswell.

--- remember_me.py
+++ remember_me.py
@@ -1,19 +1,27 @@
 import json
 
 
-def greet_user():
+def get_stored_username():
     filename = 'username.json'
     try:
         with open(filename) as f:
             username = json.load(f)
     except FileNotFoundError:
-        username = input("What is your name? ")
+        return None
+    else:
+        return username
 
+
+def greet_user():
+    username = get_stored_username()
+    if username:
+        print(f"Welcome back, {username}!")
+    else:
+        username = input("What is your name? ")
+        filename = "username.json"
         with open(filename, "w+") as f:
             json.dump(username, f)
             print(f"We'll remember you when you come back, {username}!")
-    else:
-        print(f"Welcome back, {username}!")
 
 
 greet_user()
