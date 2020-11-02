d343b4be408e82de945a92ac46e9bea8bd5fadc6
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-11-02 14:08:33 -0500

    Added another way to print each line.

--- file_reader.py
+++ file_reader.py
@@ -1,5 +1,8 @@
 filename = 'pi_digits.txt'
 
 with open(filename) as file_object:
-    for line in file_object:
-        print(line)
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
+
