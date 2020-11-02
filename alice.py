e1fc6cf17615f4d6faff26dbefc193376f6d3be3
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-11-02 14:28:01 -0500

    Added a catch for the filenotfounderror.

--- alice.py
+++ alice.py
@@ -1,4 +1,8 @@
 filename = 'alice.txt'
 
-with open(filename, encoding='utf-8') as f:
-    contents = f.read()
+try:
+    with open(filename, encoding='utf-8') as f:
+        contents = f.read()
+except FileNotFoundError:
+    print(f"Sorry, the file {filename} does not exist.")
+
