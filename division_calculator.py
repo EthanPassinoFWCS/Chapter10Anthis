2e9b91b8bdda2642543bb3103b8072ba9a83f49a
Ethan Passino <ethan.passino@fwcsstudents.org>
2020-11-02 14:23:44 -0500

    Rewrote all of the code.
    Now takes in two numbers and divides them both and prints the result.
    Keeps asking for input until you put "q" for input.
    This will error if you do not put a valid number or put 0 for the second number.

--- division_calculator.py
+++ division_calculator.py
@@ -1,4 +1,13 @@
-try:
-    print(5/0)
-except ZeroDivisionError:
-    print("You cannot divide by zero!")
+print("Give me two numbers, and I'll divide them.")
+print("Enter 'q' to quit")
+
+while True:
+    first_number = input("\nFirst Number: ")
+    if first_number == "q":
+        break
+    second_number = input("Second Number: ")
+    if second_number == "q":
+        break
+
+    answer = int(first_number) / int(second_number)
+    print(answer)
